01a2cc6331bcbec7927b9074f41f4fc4fdd64e40
chenskiy_vladislav <chenskiy.vlad@mail.ru>
2022-03-05 00:36:42 +0300

    Закончил 1 задачу

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,13 +1,18 @@
-﻿Console.Write("Введите первое целое число ");
+﻿/*
+// 1 задача
+Console.Write("Введите первое целое число ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе целое число ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 if(a > b)
 {
-    Console.WriteLine("max = ");
+    Console.Write("max = ");
+    Console.Write(a);
 }
 else
 {
-    Console.WriteLine("max = ");
+    Console.Write("max = ");
+    Console.Write(b);
 }
+*/
