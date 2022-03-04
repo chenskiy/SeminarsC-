815b11051f125eb5f93b1d09b81f563e63cfec03
chenskiy_vladislav <chenskiy.vlad@mail.ru>
2022-03-04 23:05:50 +0300

    1 excercise done

---
+++ Seminar_C#_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat_value = value * value;
+
+Console.Write("Квадрат данного числа равен ");
+Console.WriteLine(kvadrat_value);
