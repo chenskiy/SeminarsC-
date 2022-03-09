00d41b04fc526093a48098dc0fcb03d6f7de9d71
chenskiy_vladislav <chenskiy.vlad@mail.ru>
2022-03-09 15:00:13 +0300

    дз2 выполнено

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -35,4 +35,105 @@ AcDc(number);
 */
 
 
-/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. */
+/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.  
+// Как во втором семинаре
+
+void Day(int num)
+{
+    if(num <= 7 && num >= 1)
+    {
+        if(num == 1)
+        {
+            Console.Write("Понедельник");
+        }
+        if(num == 2)
+        {
+        Console.Write("Вторник");
+        }
+        if(num == 3)
+        {
+            Console.Write("Среда");
+        }
+        if(num == 4)
+        {
+            Console.Write("Четверг");
+        }
+        if(num == 5)
+        {
+            Console.Write("Пятница");
+        }
+        if(num == 6)
+        {
+            Console.Write("Суббота");
+        }
+        if(num == 7)
+        {
+            Console.Write("Воскресенье");
+        }
+        if(num == 6 ^ num == 7)
+        {
+            Console.Write(" - это выходной день!");
+        }
+        else
+        {
+            Console.Write(" - это рабочий день!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Такого дня недели не существует");
+    }
+}
+
+Console.WriteLine("Введите номер дня недели (от 1 до 7) ");
+int num = Convert.ToInt32(Console.ReadLine());
+Day(num);
+*/
+
+/* //Как в первом семинаре
+Console.WriteLine("Введите номер дня недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number <= 7 && number >= 1)
+{
+    if(number == 1)
+    {
+        Console.Write("Понедельник");
+    }
+    if(number == 2)
+    {
+    Console.Write("Вторник");
+    }
+    if(number == 3)
+    {
+        Console.Write("Среда");
+    }
+    if(number == 4)
+    {
+        Console.Write("Четверг");
+    }
+    if(number == 5)
+    {
+        Console.Write("Пятница");
+    }
+    if(number == 6)
+    {
+        Console.Write("Суббота");
+    }
+    if(number == 7)
+    {
+        Console.Write("Воскресенье");
+    }
+    if(number == 6 ^ number == 7)
+    {
+        Console.Write(" - это выходной день!");
+    }
+    else
+    {
+        Console.Write(" - это рабочий день!");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+*/
